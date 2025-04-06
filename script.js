a5af73157f9e7a990d671af7daf0baf45995d416
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-04-06 20:53:44 +0400

    Fix grid, Add custom dimensions and style
    Fixed the grid system. added styling and user can change the dimensions

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
 let container = document.querySelector("#container");
 let colorSelector = document.querySelector("#color-selector");
+let dimension = document.querySelector("#dimensions");
+let enterBtn = document.querySelector("#enter-btn");
 let mouseDown = false;
 
 document.body.onmousedown = () => { 
     mouseDown = true;
-    console.log(mouseDown);
 }
 document.body.onmouseup = () => {
     mouseDown = false;
-    console.log(mouseDown)
 }
 
-for (let i = 1; i < 16*16; i++) {
-    let pixel = document.createElement("div");
-    pixel.classList.add("pixel");
-    container.appendChild(pixel);
-}
-
-let pixels = document.querySelectorAll(".pixel");
-
-for (let i = 0; i <= pixels.length; i++) {
-    if (mouseDown) pixels[i].style.background = colorSelector.value;
-    pixels[i]?.addEventListener("mouseover", () => {
-        if (mouseDown) {
-            pixels[i].style.background = colorSelector.value;
+function loadPixels() {
+    for (let i = 1; i <= dimension.value; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        
+        for (let j = 1; j <= dimension.value; j++) {
+            let pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+            row.appendChild(pixel);
         }
+    }
+}
+
+function drawing() {
+    let pixels = document.querySelectorAll(".pixel");
+
+    pixels.forEach(pixel => {
+        pixel?.addEventListener("mousedown", () => {
+            pixel.style.background=colorSelector.value;
+        })
+
+        pixel?.addEventListener("mouseover", () => {
+            if (mouseDown) {
+                pixel.style.background = colorSelector.value;
+            }
+        })
+    })
+}
+
+loadPixels();
+drawing();
+
+enterBtn.addEventListener("click", ()=> {
+    //remove current pixels
+    let divs = document.querySelectorAll("#container > div");
+    Array.from(divs).forEach(element => {
+        element.remove();
     })
 
-}
+    loadPixels();
+    drawing();
+});
